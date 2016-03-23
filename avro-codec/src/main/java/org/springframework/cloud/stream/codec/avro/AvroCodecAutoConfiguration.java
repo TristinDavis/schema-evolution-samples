a2ed42fd914163466387ec339e25af9c4a367346
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2016-03-23 14:36:31 -0400

    Adding cachingschemaregistry fixing toc on readme

--- avro-codec/src/main/java/org/springframework/cloud/stream/codec/avro/AvroCodecAutoConfiguration.java
+++ avro-codec/src/main/java/org/springframework/cloud/stream/codec/avro/AvroCodecAutoConfiguration.java
@@ -7,11 +7,13 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.stream.schema.CachingSchemaRegistryClient;
 import org.springframework.cloud.stream.schema.ConfluentSchemaRegistryClient;
 import org.springframework.cloud.stream.schema.SchemaRegistryClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.integration.codec.Codec;
 
 /**
@@ -34,9 +36,15 @@ public class AvroCodecAutoConfiguration {
 
 	@Bean
 	@ConditionalOnProperty("confluent.schemaregistry.endpoint")
-	public SchemaRegistryClient schemaRegistryClient(@Value("${confluent.schemaregistry.endpoint}") String endpoint){
+	public SchemaRegistryClient confluentClient(@Value("${confluent.schemaregistry.endpoint}") String endpoint){
 		ConfluentSchemaRegistryClient registryClient = new ConfluentSchemaRegistryClient(endpoint);
 		return registryClient;
+	}
+
+	@Bean
+	@Primary
+	public SchemaRegistryClient schemaRegistryClient(SchemaRegistryClient client){
+		return new CachingSchemaRegistryClient(client);
 	}
 
 
---
+++ avro-codec/src/main/java/org/springframework/cloud/stream/schema/CachingSchemaRegistryClient.java
@@ -0,0 +1,49 @@
+package org.springframework.cloud.stream.schema;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.apache.avro.Schema;
+
+/**
+ * @author Vinicius Carvalho
+ */
+public class CachingSchemaRegistryClient implements SchemaRegistryClient{
+
+	private ConcurrentHashMap<Integer,Schema> schemaCache = new ConcurrentHashMap<>();
+	private SchemaRegistryClient delegate;
+
+	public CachingSchemaRegistryClient(SchemaRegistryClient delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	public Integer register(Schema schema) {
+		Integer id = findSchema(schema);
+		if(id == null){
+			id = delegate.register(schema);
+			schemaCache.put(id,schema);
+		}
+		return id;
+	}
+
+	@Override
+	public Schema fetch(Integer id) {
+		Schema schema = schemaCache.get(id);
+		if(schema == null){
+			schema = delegate.fetch(id);
+			schemaCache.put(id,schema);
+		}
+		return schema;
+	}
+
+	private Integer findSchema(Schema schema){
+		for(Map.Entry<Integer,Schema> entry : schemaCache.entrySet()){
+			if(entry.getValue().equals(schema))
+				return entry.getKey();
+		}
+		return null;
+	}
+}
